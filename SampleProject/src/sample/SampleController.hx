14e8fb62b995ccc343ea2ca89e7a4b3cdbfbaae2
Emiliano Ariel Carmona <elcarmon@gmail.com>
2014-08-11 20:15:08 -0300

    SampleProject added.

---
+++ SampleProject/src/sample/SampleController.hx
@@ -0,0 +1,19 @@
+package sample;
+
+import crossfailure.controllers.MainController;
+import flash.display.DisplayObjectContainer;
+
+/**
+ * ...
+ * @author Carmon
+ */
+class SampleController extends MainController
+{
+
+	public function new(doc:DisplayObjectContainer, ?model:Dynamic) 
+	{
+		super(doc, ?model);
+		
+	}
+	
+}
